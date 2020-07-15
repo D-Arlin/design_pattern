a08b5f827947a5f2fb261f6d5fe004a31bc079f9
D-Arlin <356230410@qq.com>
2020-07-15 15:44:30 +0800

    图书源码

---
+++ src/Adapter/Q2/Main.java
@@ -0,0 +1,21 @@
+package Adapter.Q2;
+
+import Adapter.A2.FileProperties;
+
+import java.io.IOException;
+
+
+public class Main {
+    public static void main(String[] args) {
+        FileIO f = (FileIO) new FileProperties();
+        try {
+            f.readFromFile("file.txt");
+            f.setValue("year", "2004");
+            f.setValue("month", "4");
+            f.setValue("day", "21");
+            f.writeToFile("newfile.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
